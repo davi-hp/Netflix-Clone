3e46e1a7ddc963571fe62069c168718abfda559b
davi-hp <davitrajano473@gmail.com>
2022-01-23 20:31:07 -0300

    Parte 7

---
+++ js/owl/setup.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:10,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        },
+    }
+});
